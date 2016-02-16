cce9cf3f641ec028361a476a66915878aafe083c
Maciej Latt <macieklatt@gmail.com>
2016-02-16 23:56:45 +0100

    Dodanie brakujacych plikow i uporzadkowanie folderu repozytorium

---
+++ footer.php
@@ -0,0 +1,25 @@
+	<!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; Bartosz Soczyński &amp; Maciej Latt 2016</p>
+                </div>
+            </div>
+            <!-- /.row -->
+        </footer>
+
+    </div>
+    <!-- /.container -->
+
+ 	<!-- jQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Script to Activate the Carousel -->
+    <script>
+    $('.carousel').carousel({
+        interval: 5000 //changes the speed
+    })
+    </script>
---
+++ header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pl_PL">
+	<head>
+		<meta charset="utf-8">
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+	    <title>TTR 2016</title>
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link href="style.css" rel="stylesheet">
+		<!-- Custom CSS -->
+    	<link href="css/full-slider.css" rel="stylesheet">
+	</head>
+	
+	<body>
+		<!-- Navigation -->
+		<nav class="navbar navbar-default navbar-fixed-top">
+	      <div class="container">
+	        <div class="navbar-header">
+	          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+	            <span class="sr-only">Nawigacja</span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	          </button>
+	          <a class="navbar-brand" href="#"><img src="images/Logo.jpg"></a>
+	        </div>
+	        <div id="navbar" class="navbar-collapse collapse">
+	          <ul class="nav navbar-nav navbar-right">
+		         <li><a href="oturnieju.html">O turnieju</a></li>
+		         <li><a href="partnerzy.html">Partnerzy</a></li>
+		         <li><a href="strefa.html">Strefa zawodników</a></li>
+		         <li><a href="kontakt.html">Kontakt</a></li>
+	          </ul>
+	        </div><!--/.nav-collapse -->
+	      </div>
+	    </nav>
